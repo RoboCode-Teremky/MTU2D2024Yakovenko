cde6ba01a710b3dee89a488fee8a2b64927687c7
Oleksiy <robocode.teremky.students@gmail.com>
2024-06-09 19:59:48 +0300

    ex2

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,24 +7,45 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] private float speed = 1.0f;
     [SerializeField] private float jumpForce = 10.0f;
     private Rigidbody2D rigidbody2D;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate()
     {
         float moveX = Input.GetAxis("Horizontal");
 
+    Flip();
+
         foreach (RaycastHit2D raycastHit2D in Physics2D.RaycastAll(transform.position, Vector2.down, 1.5f))
         {
             if (raycastHit2D.collider.CompareTag("Ground") && Input.GetButton("Jump"))
             {
                 rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                animator.SetTrigger("JumpT");
             }
         }
 
         rigidbody2D.velocity = new Vector2(moveX * speed, rigidbody2D.velocity.y);
+        animator.SetFloat("SpeedX", Mathf.Abs(rigidbody2D.velocity.x));
+        animator.SetFloat("SpeedY", rigidbody2D.velocity.y);
+    }
+    void Flip (){
+        if (Input.GetAxis("Horizontal")<0){
+            //transform.localRotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+            spriteRenderer.flipX = true;
+        }
+        if (Input.GetAxis("Horizontal")>0){
+            //transform.localRotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
+            spriteRenderer.flipX = false;
+        }
+
+
     }
 
     private void OnDrawGizmos()
