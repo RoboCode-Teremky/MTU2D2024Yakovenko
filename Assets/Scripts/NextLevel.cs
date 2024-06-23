134164fb347b3c73b46b9c7b254dc1236867cd43
Oleksiy <robocode.teremky.students@gmail.com>
2024-06-23 13:50:40 +0300

    23.06.24

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class NextLevel : MonoBehaviour
+{
+
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    }
+    void OnTriggerEnter2D(Collider2D colision){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+    }
